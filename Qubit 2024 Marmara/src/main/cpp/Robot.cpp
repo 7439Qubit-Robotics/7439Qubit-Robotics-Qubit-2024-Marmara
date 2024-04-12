bc3ac5d6fb82f54b65a7a8467d2be4fdffe560cf
Poyraz Öztok <149818616+poyraz-oztok@users.noreply.github.com>
2024-04-12 22:23:08 +0300

    Upluaded Robot.cpp
    Corrected Robot.cpp

--- Qubit 2024 Marmara/src/main/cpp/Robot.cpp
+++ Qubit 2024 Marmara/src/main/cpp/Robot.cpp
@@ -1,75 +1,229 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
-#include "Robot.h"
-
-#include <fmt/core.h>
+#include <frc/Joystick.h>
+#include <frc/TimedRobot.h>
+#include <frc/drive/MecanumDrive.h>
+#include <frc/motorcontrol/PWMSparkMax.h>
+#include <frc/motorcontrol/VictorSP.h>
+#include <frc/Timer.h>
+#include <frc/PS4Controller.h>
 #include <frc/smartdashboard/SmartDashboard.h>
+#include <cameraserver/CameraServer.h>  
 
-void Robot::RobotInit() {
-  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
-  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
-  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
-}
 
-/**
- * This function is called every 20 ms, no matter the mode. Use
- * this for items like diagnostics that you want ran during disabled,
- * autonomous, teleoperated and test.
- *
- * <p> This runs after the mode specific periodic functions, but before
- * LiveWindow and SmartDashboard integrated updating.
- */
-void Robot::RobotPeriodic() {}
 
-/**
- * This autonomous (along with the chooser code above) shows how to select
- * between different autonomous modes using the dashboard. The sendable chooser
- * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
- * remove all of the chooser code and uncomment the GetString line to get the
- * auto name from the text box below the Gyro.
- *
- * You can add additional auto modes by adding additional comparisons to the
- * if-else structure below with additional strings. If using the SendableChooser
- * make sure to add them to the chooser code above as well.
- */
-void Robot::AutonomousInit() {
-  m_autoSelected = m_chooser.GetSelected();
-  // m_autoSelected = SmartDashboard::GetString("Auto Selector",
-  //     kAutoNameDefault);
-  fmt::print("Auto selected: {}\n", m_autoSelected);
+bool brake;
 
-  if (m_autoSelected == kAutoNameCustom) {
-    // Custom Auto goes here
-  } else {
-    // Default Auto goes here
+int shoot;
+int intake;
+
+
+
+class Robot : public frc::TimedRobot {
+
+ public:
+
+  void RobotInit() override {
+    wpi::SendableRegistry::AddChild(&m_robotDrive, &m_frontLeft);
+    wpi::SendableRegistry::AddChild(&m_robotDrive, &m_rearLeft);
+    wpi::SendableRegistry::AddChild(&m_robotDrive, &m_frontRight);
+    wpi::SendableRegistry::AddChild(&m_robotDrive, &m_rearRight);
+    
+
+    greyInTake2.SetInverted(true);
+    Arm2.SetInverted(false);
+    greyInTake.SetInverted(true);
+    m_frontLeft.SetInverted(true);
+    m_rearLeft.SetInverted(true);
+    greyInTake2.SetInverted(true);
+    greenInTake.SetInverted(true);
   }
-}
 
-void Robot::AutonomousPeriodic() {
-  if (m_autoSelected == kAutoNameCustom) {
-    // Custom Auto goes here
-  } else {
-    // Default Auto goes here
+  void brakes(){
+    Arm.Set(0.05);
+    Arm2.Set(0.0625);
   }
-}
 
-void Robot::TeleopInit() {}
+  void RobotPeriodic() override{
+    frc::SmartDashboard::PutBoolean("Brakes",brake);
+    frc::SmartDashboard::PutNumber("Shooters", shoot);
+    frc::SmartDashboard::PutNumber("inTakers", intake);
+  }
 
-void Robot::TeleopPeriodic() {}
+  void DisabledPeriodic() override {
+      Arm.Set(0.065); //Controlling the Arm
+      Arm2.Set(0.075);
+  }
 
-void Robot::DisabledInit() {}
+  void TeleopPeriodic() override {
 
-void Robot::DisabledPeriodic() {}
+    m_robotDrive.DriveCartesian(JoyDrv.GetRawAxis(1)/*Ileri-Geri*/, -JoyDrv.GetRawAxis(0)/*Sağ-Sol*/,
+                                -JoyDrv.GetRawAxis(4)*0.5/*Donuş*/);
 
-void Robot::TestInit() {}
+  if(JoyDrv.GetRawButton(2)==true){ //Shooting the note
+      greenInTake.Set(-1);
+      intake = 1;
+   }
+    else if(JoyDrv.GetRawButton(3)==true) {
+      greyInTake.Set(1);
+      greyInTake2.Set(1);
+      shoot = -1;
+  }
+  
+  else if(JoyDrv.GetRawButton(1)){
+    greenInTake.Set(1);
+    intake = -1;
+  }
 
-void Robot::TestPeriodic() {}
+  else if(JoyDrv.GetRawButton(4)){ 
+      greyInTake.Set(-1);
+      greyInTake2.Set(-1);
+      shoot = 1;
+  }
 
-void Robot::SimulationInit() {}
+  else if(JoyDrv.GetRawButton(10)) {
+    greenInTake.Set(-0.5);
+    greyInTake2.Set(-0.5);
+    greyInTake.Set(-0.5);
+  }
 
-void Robot::SimulationPeriodic() {}
+  else{
+      greenInTake.Set(0);
+      greyInTake.Set(0);
+      greyInTake2.Set(0);
+      intake = 0;
+      shoot= 0;
+  }
+  
+
+        if(JoyDrv.GetRawButton(6)){
+        Arm.Set(1*0.1); //Controlling the Arm
+        Arm2.Set(1*0.25);
+        brake = false;
+      }
+
+      else if(JoyDrv.GetRawButton(5)){
+        Arm.Set(-1*0.2); //Controlling the Arm
+        Arm2.Set(-1*0.25);
+        brake = false;
+      }
+
+      else if(JoyDrv.GetRawButtonPressed(5) == false && JoyDrv.GetRawButtonPressed(6) == false){
+        brake=true;
+        Arm.Set(0.065); //Controlling the Arm
+        Arm2.Set(0.075);
+      }
+  }
+
+  void AutonomousInit() override{
+    timer.Reset();
+    timer.Start();
+  }
+
+  void AutonomousPeriodic() override {
+    if(timer.Get() < 2000_ms && timer.Get()> 0_s){ //Kol iner ve shooterlar çalışır
+      Arm.Set(-1*0.3);
+      Arm2.Set(-1*0.35);
+      greyInTake.Set(-1);
+      greyInTake2.Set(-1);
+    }
+    
+    else if(timer.Get() > 2600_ms && timer.Get() < 4.3_s){//halka ateşlenir
+      greyInTake.Set(-1);
+      greyInTake2.Set(-1);
+      greenInTake.Set(-1);
+      
+      brakes();
+    }
+
+    else if(timer.Get() > 4.3_s && timer.Get() < 5.7_s){//Kol iner
+      Arm.Set(-1*0.3);
+      Arm2.Set(-1*0.25);
+      greyInTake.Set(-1);
+      greyInTake2.Set(-1);
+    }
+    
+    else if (timer.Get() > 5.7_s && timer.Get() < 6.6_s){//şase geri
+      m_robotDrive.DriveCartesian(0.4, 0, 0);
+      Arm.Set(0);
+      Arm2.Set(0);
+      greyInTake2.Set(0);
+      greyInTake.Set(0);
+      brakes();
+    }
+
+    else if (timer.Get() > 6.6_s && timer.Get() < 6.9_s){//alış
+      greenInTake.Set(-1);
+/*       Arm.Set(1*0.2);
+      Arm2.Set(1*0.25); */
+    }
+
+    else if (timer.Get() > 6.9_s && timer.Get() < 8.2_s){ //Şase ileri
+      Arm.Set(1*0.2);
+      Arm2.Set(1*0.25);
+      greenInTake.Set(0);
+    }
+
+    else if (timer.Get() > 10.2_s && timer.Get() < 11.2_s){//Kol yukarı
+      m_robotDrive.DriveCartesian(-0.4, -0.0, 0);
+      Arm.Set(0);
+      Arm2.Set(0);
+      greenInTake.Set(0);
+      brakes();
+    }
+
+    else if(timer.Get() > 11.2_s && timer.Get() < 12.2_s){ //charge
+      greyInTake.Set(-1);
+      greyInTake2.Set(-1);
+      brakes();
+    }
+    
+    else if(timer.Get() > 12.2_s && timer.Get() < 13.3_s){//Atış
+
+      greenInTake.Set(-1);
+      brakes();
+    }
+
+    else if(timer.Get() > 13.3_s && timer.Get() < 15_s){//Çizgiden çıkış
+      m_robotDrive.DriveCartesian(0.5, 0, 0);
+
+    }
+    else{
+      m_robotDrive.DriveCartesian(0, 0, 0);
+      greenInTake.Set(0);
+      greyInTake2.Set(0);
+      greyInTake.Set(0);
+      brakes();
+    }
+  }
+
+ private:
+  frc::Timer timer;
+
+  static constexpr int kFrontLeftChannel = 1;
+  static constexpr int kRearLeftChannel = 3;
+  static constexpr int kFrontRightChannel = 0;
+  static constexpr int kRearRightChannel = 2;
+
+  static constexpr int kJoystickChannel = 0;
+
+  frc::VictorSP m_frontLeft{kFrontLeftChannel};
+  frc::VictorSP m_rearLeft{kRearLeftChannel};
+  frc::VictorSP m_frontRight{kFrontRightChannel};
+  frc::VictorSP m_rearRight{kRearRightChannel};
+  frc::MecanumDrive m_robotDrive{
+      [&](double output) { m_frontLeft.Set(output); },
+      [&](double output) { m_rearLeft.Set(output); },
+      [&](double output) { m_frontRight.Set(output); },
+      [&](double output) { m_rearRight.Set(output); }
+      };
+      
+  frc::VictorSP Arm{4};
+  frc::PWMSparkMax greenInTake{8};
+  frc::VictorSP greyInTake{7};
+  frc::VictorSP greyInTake2{6};
+  frc::PWMSparkMax Arm2{5};
+
+  frc::Joystick JoyDrv{0};
+};
 
 #ifndef RUNNING_FRC_TESTS
 int main() {
